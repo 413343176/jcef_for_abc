9c1c3f8ad12032c4c2770c97cefeabaea5d6e978
sky <lsj2820@163.com>
2019-01-24 12:43:41 +0800

    增加2171后的N_PreInitialize方法，防止运行崩溃

--- java-cef/src/src/java/org/cef/CefApp.java
+++ java-cef/src/src/java/org/cef/CefApp.java
@@ -157,6 +157,10 @@ public class CefApp extends CefAppHandlerAdapter {
     if (appHandler_ == null) {
       appHandler_ = this;
     }
+    
+    if (!N_PreInitialize()) {
+    	throw new IllegalStateException("Failed to pre-initialize native code");
+    }
 
     // On Mac we're registering a shutdown hook to shutdown the native CEF
     // part. This is useful if it is missed to call CefApp.disopse() before
@@ -606,7 +610,8 @@ public class CefApp extends CefAppHandlerAdapter {
     }
     return library_path;
   }
-
+  
+  private final native boolean N_PreInitialize();
   private final native boolean N_Initialize(String pathToJavaDLL,
       CefAppHandler appHandler, CefSettings settings);
   private final native void N_Shutdown();
